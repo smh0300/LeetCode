2ddc50f739c45a5d3c6bbd83b73ccc2688e6da7b
Son <57385998+smh0300@users.noreply.github.com>
2023-05-02 18:08:12 +0900

    Time: 52 ms (5.40%), Space: 16.3 MB (6.08%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,15 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        
+        if n==1:
+            return 1
+        
+        ans=[0] * n
+        print(n)
+        ans[0]=1
+        ans[1]=1
+
+        for x in range(2,n):
+            ans[x] = ans[x-2]+ans[x-1]
+        
+        return ans[-1]+ans[-2]
