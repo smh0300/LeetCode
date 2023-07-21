99af21c61ccc64ba4780957482276d29b75abb20
Son <57385998+smh0300@users.noreply.github.com>
2023-07-21 23:38:35 +0900

    Time: 113 ms (5.26%), Space: 16.7 MB (90.30%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.py
+++ 0841-keys-and-rooms/0841-keys-and-rooms.py
@@ -1,18 +1,14 @@
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
-        def traversal(room):
-            keys = deque()
-            for _ in rooms[0]:
-                keys.append(_)
-            visit = [0]*len(room)
-            visit[0] = 1
-            while keys:
-                x = keys.popleft()
-                if visit[x] == 0:
-                    visit[x] = 1
-                    for _ in rooms[x]:
-                        keys.append(_)
-            return sum(visit)
-
-        success = traversal(rooms)
-        return True if success == len(rooms) else False
+        keys = list(set([x for x in rooms[0]]))
+        visit = [0]
+        
+        while keys:
+            next_key = keys.pop()
+            if next_key not in visit:
+                visit.append(next_key)
+                new_keys = [x for x in rooms[next_key] if x not in visit]
+                keys.extend(new_keys)
+                
+        return True if len(rooms)==len(visit) else False
+        
