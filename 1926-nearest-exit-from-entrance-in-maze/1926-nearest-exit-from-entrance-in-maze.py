d93a3fbc1c19c26719a28a3f7d2a8bff4088fa6f
Son <57385998+smh0300@users.noreply.github.com>
2023-07-22 22:50:10 +0900

    Time: 855 ms (66.46%), Space: 18 MB (41.74%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.py
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.py
@@ -1,36 +1,26 @@
 class Solution:
     def nearestExit(self, maze: List[List[str]], entrance: List[int]) -> int:
-        rows, cols = len(maze), len(maze[0])
-        dirs = ((1, 0), (-1, 0), (0, 1), (0, -1))
-        
-        # Mark the entrance as visited since its not a exit.
-        start_row, start_col = entrance
-        maze[start_row][start_col] = "+"
-        
-        # Start BFS from the entrance, and use a queue `queue` to store all 
-        # the cells to be visited.
-        queue = collections.deque()
-        queue.append([start_row, start_col, 0])
-        
-        while queue:
-            curr_row, curr_col, curr_distance = queue.popleft()
-            
-            # For the current cell, check its four neighbor cells.
-            for d in dirs:
-                next_row = curr_row + d[0]
-                next_col = curr_col + d[1]
-                
-                # If there exists an unvisited empty neighbor:
-                if 0 <= next_row < rows and 0 <= next_col < cols \
-                    and maze[next_row][next_col] == ".":
-                    
-                    # If this empty cell is an exit, return distance + 1.
-                    if 0 == next_row or next_row == rows - 1 or 0 == next_col or next_col == cols - 1:
-                        return curr_distance + 1
-                    
-                    # Otherwise, add this cell to 'queue' and mark it as visited.
-                    maze[next_row][next_col] = "+"
-                    queue.append([next_row, next_col, curr_distance + 1])
-            
-        # If we finish iterating without finding an exit, return -1.
+        m = len(maze)
+        n = len(maze[0])
+        dirs = [0, 1, 0, -1, 0]
+        ans = 0
+        q = collections.deque([(entrance[0], entrance[1])])
+        seen = {(entrance[0], entrance[1])}
+
+        while q:
+          ans += 1
+          for _ in range(len(q)):
+            i, j = q.popleft()
+            for k in range(4):
+              x = i + dirs[k]
+              y = j + dirs[k + 1]
+              if x < 0 or x == m or y < 0 or y == n:
+                continue
+              if (x, y) in seen or maze[x][y] == '+':
+                continue
+              if x == 0 or x == m - 1 or y == 0 or y == n - 1:
+                return ans
+              q.append((x, y))
+              seen.add((x, y))
+
         return -1
